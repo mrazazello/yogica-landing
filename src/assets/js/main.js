a06d484d84312a06f1ff80155ebc52aa7f76e253
Bakhriev <muradbakhriev@gmail.com>
2024-03-27 18:13:10 +0500

    fix problems

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -16,3 +16,29 @@ overlay.addEventListener('click', () => {
 	overlay.classList.remove('active');
 	body.classList.remove('no-scroll');
 });
+
+const swiper = new Swiper('.session__carousel', {
+	// If we need pagination
+	slidesPerView: 1,
+	spaceBetween: 10,
+	pagination: {
+		el: '.swiper-pagination',
+	},
+
+	// Navigation arrows
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
+	},
+
+	breakpoints: {
+		500: {
+			slidesPerView: 3,
+			spaceBetween: 20,
+		},
+		992: {
+			slidesPerView: 6,
+			spaceBetween: 20,
+		},
+	},
+});
